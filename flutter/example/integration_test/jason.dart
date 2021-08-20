c8ce7d7adb0aa06fd7884472f5e1134a5b330137
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-08-20 10:19:51 -0500

    Fix VERY stupid Flutter tests failures

--- flutter/example/integration_test/jason.dart
+++ flutter/example/integration_test/jason.dart
@@ -79,7 +79,7 @@ void main() {
             e is LocalMediaInitException &&
             e.kind == LocalMediaInitExceptionKind.GetUserMediaFailed &&
             e.cause == 'Dart err cause1' &&
-            e.nativeStackTrace.contains('at jason/src'))));
+            e.nativeStackTrace.contains('at src'))));
 
     var err;
     try {
@@ -94,14 +94,14 @@ void main() {
             e is LocalMediaInitException &&
             e.kind == LocalMediaInitExceptionKind.GetDisplayMediaFailed &&
             e.cause == 'Dart err cause2' &&
-            e.nativeStackTrace.contains('at jason/src')));
+            e.nativeStackTrace.contains('at src')));
 
     expect(
         () => returnsEnumerateDevicesException('Dart err cause3').unwrap(),
         throwsA(predicate((e) =>
             e is EnumerateDevicesException &&
             e.cause == 'Dart err cause3' &&
-            e.nativeStackTrace.contains('at jason/src'))));
+            e.nativeStackTrace.contains('at src'))));
 
     var err2;
     try {
@@ -115,7 +115,7 @@ void main() {
         predicate((e) =>
             e is EnumerateDevicesException &&
             e.cause == 'Dart err cause4' &&
-            e.nativeStackTrace.contains('at jason/src')));
+            e.nativeStackTrace.contains('at src')));
   });
 
   testWidgets('DeviceVideoTrackConstraints', (WidgetTester tester) async {
@@ -351,7 +351,7 @@ void main() {
         predicate((e) =>
             e is MediaStateTransitionException &&
             e.message == 'SimpleTracksRequest should have at least one track' &&
-            e.nativeStackTrace.contains('at jason/src')));
+            e.nativeStackTrace.contains('at src')));
   });
 
   testWidgets('ReconnectHandle', (WidgetTester tester) async {
@@ -421,7 +421,7 @@ void main() {
             e.kind == RpcClientExceptionKind.ConnectionLost &&
             e.cause == 'Dart err cause1' &&
             e.message == 'RpcClientException::ConnectionLost' &&
-            e.nativeStackTrace.contains('at jason/src'))));
+            e.nativeStackTrace.contains('at src'))));
 
     var exception5;
     try {
@@ -436,7 +436,7 @@ void main() {
             e.kind == RpcClientExceptionKind.SessionFinished &&
             e.message == 'RpcClientException::SessionFinished' &&
             e.cause == 'Dart err cause2' &&
-            e.nativeStackTrace.contains('at jason/src')));
+            e.nativeStackTrace.contains('at src')));
   });
 
   final returnsInputDevicePtr =
