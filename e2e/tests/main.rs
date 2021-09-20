882ff8ca070bec40f26b420f4146bbd2e075044a
ilslv <ilya.solovyiov@gmail.com>
2021-09-20 12:51:36 +0300

    Fallback to serial execution [run ci]

--- e2e/tests/main.rs
+++ e2e/tests/main.rs
@@ -5,7 +5,11 @@ mod control;
 mod steps;
 mod world;
 
-use cucumber_rust::WorldInit as _;
+use std::sync::atomic::Ordering;
+
+use atomic::Atomic;
+use cucumber_rust::{World as _, WorldInit as _};
+use once_cell::sync::Lazy;
 use regex::Regex;
 use structopt::StructOpt;
 
@@ -21,14 +25,37 @@ struct Conf {
 async fn main() {
     let conf: Conf = Conf::from_args();
 
+    let concurrent = supports_multiple_webdriver_clients()
+        .await
+        .then(|| 10)
+        .unwrap_or(1);
+
     let filter = conf.scenario.as_ref().map(|s| Regex::new(s).unwrap());
     World::cucumber()
         .fail_on_skipped()
-        .max_concurrent_scenarios(Some(10))
+        .max_concurrent_scenarios(concurrent)
         .filter_run_and_exit(conf::FEATURES_PATH.as_str(), move |_, _, s| {
             filter
                 .as_ref()
                 .map_or(true, |filter| filter.find(&s.name).is_some())
         })
         .await;
+}
+
+/// Indicates whether `WebDriver` implementation supports multiple simultaneous
+/// connections.
+///
+/// This is done, because `geckodriver` [doesn't support this feature][1]
+///
+/// [1]: https://github.com/mozilla/geckodriver/issues/1523
+async fn supports_multiple_webdriver_clients() -> bool {
+    static VALUE: Lazy<Atomic<Option<bool>>> = Lazy::new(|| Atomic::new(None));
+
+    if let Some(v) = VALUE.load(Ordering::SeqCst) {
+        v
+    } else {
+        let v = World::new().await.and(World::new().await).is_ok();
+        VALUE.store(Some(v), Ordering::SeqCst);
+        v
+    }
 }
