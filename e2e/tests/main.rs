3ef2d282c835e753f9afab1f6f42be729de56b41
ilslv <ilya.solovyiov@gmail.com>
2021-09-21 16:08:32 +0300

    Corrections [run ci]

--- e2e/tests/main.rs
+++ e2e/tests/main.rs
@@ -5,11 +5,7 @@ mod control;
 mod steps;
 mod world;
 
-use std::sync::atomic::Ordering;
-
-use atomic::Atomic;
 use cucumber_rust::{World as _, WorldInit as _};
-use once_cell::sync::Lazy;
 use regex::Regex;
 use structopt::StructOpt;
 
@@ -35,27 +31,18 @@ async fn main() {
         .fail_on_skipped()
         .max_concurrent_scenarios(concurrent)
         .filter_run_and_exit(conf::FEATURES_PATH.as_str(), move |_, _, s| {
-            filter
-                .as_ref()
-                .map_or(true, |filter| filter.find(&s.name).is_some())
+            filter.as_ref().map_or(true, |f| f.find(&s.name).is_some())
         })
         .await;
 }
 
 /// Indicates whether `WebDriver` implementation supports multiple simultaneous
-/// connections.
+/// clients.
 ///
-/// This is done, because `geckodriver` [doesn't support this feature][1]
+/// This is done, because `geckodriver` [doesn't support this feature][1], so
+/// tests won't run concurrently on `FireFox`.
 ///
 /// [1]: https://github.com/mozilla/geckodriver/issues/1523
 async fn supports_multiple_webdriver_clients() -> bool {
-    static VALUE: Lazy<Atomic<Option<bool>>> = Lazy::new(|| Atomic::new(None));
-
-    if let Some(v) = VALUE.load(Ordering::SeqCst) {
-        v
-    } else {
-        let v = World::new().await.and(World::new().await).is_ok();
-        VALUE.store(Some(v), Ordering::SeqCst);
-        v
-    }
+    World::new().await.and(World::new().await).is_ok()
 }
