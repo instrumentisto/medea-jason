1148f78c08201403378929ec9d0dc6dde834bbd6
ilslv <47687266+ilslv@users.noreply.github.com>
2021-09-23 10:03:30 +0300

    Migrate to forked Cucumber 0.10 (#3)

--- e2e/tests/main.rs
+++ e2e/tests/main.rs
@@ -5,12 +5,44 @@ mod control;
 mod steps;
 mod world;
 
-use cucumber_rust::WorldInit as _;
+use cucumber_rust::{World as _, WorldInit as _};
+use regex::Regex;
+use structopt::StructOpt;
 
 use self::world::World;
 
+#[derive(StructOpt)]
+struct Conf {
+    #[structopt(long)]
+    scenario: Option<String>,
+}
+
 #[tokio::main]
 async fn main() {
-    let runner = World::init(&[conf::FEATURES_PATH.as_str()]);
-    runner.run_and_exit().await;
+    let conf: Conf = Conf::from_args();
+
+    let concurrent = supports_multiple_webdriver_clients()
+        .await
+        .then(|| 10)
+        .unwrap_or(1);
+
+    let filter = conf.scenario.as_ref().map(|s| Regex::new(s).unwrap());
+    World::cucumber()
+        .fail_on_skipped()
+        .max_concurrent_scenarios(concurrent)
+        .filter_run_and_exit(conf::FEATURES_PATH.as_str(), move |_, _, s| {
+            filter.as_ref().map_or(true, |f| f.find(&s.name).is_some())
+        })
+        .await;
+}
+
+/// Indicates whether `WebDriver` implementation supports multiple simultaneous
+/// clients.
+///
+/// This is done, because geckodriver [doesn't support this feature][1], so
+/// tests won't run concurrently on Firefox.
+///
+/// [1]: https://github.com/mozilla/geckodriver/issues/1523
+async fn supports_multiple_webdriver_clients() -> bool {
+    World::new().await.and(World::new().await).is_ok()
 }
