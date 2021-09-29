c6878c2d96e8d7a2be45d6b4c1cae5ce83afda53
Semen Evdokimov <evdokimovs@protonmail.ch>
2021-09-29 20:00:06 +0300

    Meh, fix ptrarray

--- flutter/lib/src/util/ptrarray.dart
+++ flutter/lib/src/util/ptrarray.dart
@@ -1,14 +1,14 @@
 import 'dart:ffi';
 
 import '../native/jason.dart';
-import 'move_semantic.dart';
+import '../util/move_semantic.dart';
 
-typedef _free_C = Void Function(PtrArray);
-typedef _free_Dart = void Function(PtrArray);
+typedef _free_C = Void Function(Pointer<PtrArray>);
+typedef _free_Dart = void Function(Pointer<PtrArray>);
 
 /// Frees [PtrArray] returned from Rust.
 final _free_Dart _free =
-    dl.lookupFunction<_free_C, _free_Dart>('PtrArray_free');
+dl.lookupFunction<_free_C, _free_Dart>('PtrArray_free');
 
 /// Array of [Pointer]s to Rust objects.
 class PtrArray extends Struct {
@@ -18,15 +18,17 @@ class PtrArray extends Struct {
   /// Length of this [PtrArray].
   @Uint64()
   external int _len;
+}
 
+extension PtrArrayToList on Pointer<PtrArray> {
   /// Converts this [PtrArray] to a Dart's [List] of [Pointer]s.
   @moveSemantics
   List<Pointer> intoPointerList() {
     try {
       var out = List<Pointer>.empty(growable: true);
-      for (var i = 0; i < _len; i++) {
-        Pointer<Pointer> ptr = _ptr.elementAt(i);
-        out.add(ptr.value);
+      for (var i = 0; i < ref._len; i++) {
+        Pointer<Pointer> el = ref._ptr.elementAt(i);
+        out.add(el.value);
       }
       return out;
     } finally {
