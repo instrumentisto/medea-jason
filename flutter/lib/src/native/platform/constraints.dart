b6168f9fba6403960db8be9f514b2e47b2101b7b
evdokimovs <evdokimovs@protonmail.ch>
2022-01-24 15:27:07 +0300

    Fix video constraints

--- flutter/lib/src/native/platform/constraints.dart
+++ flutter/lib/src/native/platform/constraints.dart
@@ -27,5 +27,24 @@ void _setAudio(Constraints cons, Object val) {
 // TODO(evdokimovs): fix video constraints setting
 /// Sets [MediaStreamConstraints.video] for the provided [cons].
 void _setVideo(Constraints cons, Object val) {
-  // cons.video = val;
+  try {
+    val as Map<dynamic, dynamic>;
+    var videoConst = val['video'] as Map<dynamic, dynamic>;
+    var optional = videoConst['optional'] as Map<dynamic, dynamic>;
+    var deviceId;
+    if (optional['sourceId'] != null) {
+      deviceId = optional['sourceId'];
+    }
+    var facingMode;
+    if (optional['facingMode'] != null) {
+      if (optional['facingMode'] == 'user') {
+        facingMode = FacingMode.user;
+      } else {
+        facingMode = FacingMode.environment;
+      }
+    }
+    cons.video.optional = VideoConstraints(deviceId, facingMode);
+  } catch (e) {
+    print("Exception fuck: " + e.toString());
+  }
 }
