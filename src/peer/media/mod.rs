804872a52204a2d744e93ad520e28bd576a1d353
evdokimovs <evdokimovs@protonmail.ch>
2022-04-05 20:31:28 +0300

    Remove unrelated change

--- src/peer/media/mod.rs
+++ src/peer/media/mod.rs
@@ -6,9 +6,7 @@ pub mod receiver;
 pub mod sender;
 mod transitable_state;
 
-use std::{
-    cell::RefCell, collections::HashMap, fmt::Debug, future::Future, rc::Rc,
-};
+use std::{cell::RefCell, collections::HashMap, future::Future, rc::Rc};
 
 use derive_more::{Display, From};
 use futures::{
@@ -45,7 +43,7 @@ pub use self::{
 };
 
 /// Transceiver's sending ([`Sender`]) or receiving ([`Receiver`]) side.
-pub trait TransceiverSide: MediaStateControllable + Debug {
+pub trait TransceiverSide: MediaStateControllable {
     /// Returns [`TrackId`] of this [`TransceiverSide`].
     fn track_id(&self) -> TrackId;
 
