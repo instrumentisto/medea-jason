256143969f3009707a06b2db442a2be787276d2c
logist322 <hytornoy-99@mail.ru>
2022-05-03 15:15:08 +0400

    Fmt

--- flutter/lib/src/native/ffi/exception.g.dart
+++ flutter/lib/src/native/ffi/exception.g.dart
@@ -1,35 +1,94 @@
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:medea_jason/src/native/ffi/foreign_value.dart';
-void registerFunction(DynamicLibrary dl, {
-required Pointer<NativeFunction<Handle Function(ForeignValue, Pointer<Utf8>, Pointer<Utf8>)>> newArgumentError,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>> newStateError,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>> newFormatException,
-required Pointer<NativeFunction<Handle Function(Int64, Pointer<Utf8>, ForeignValue, Pointer<Utf8>)>> newLocalMediaInitException,
-required Pointer<NativeFunction<Handle Function(Pointer<Handle>, Pointer<Utf8>)>> newEnumerateDevicesException,
-required Pointer<NativeFunction<Handle Function(Int64, Pointer<Utf8>, ForeignValue, Pointer<Utf8>)>> newRpcClientException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>, Pointer<Utf8>, Int64)>> newMediaStateTransitionException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>, ForeignValue, Pointer<Utf8>)>> newInternalException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>, Pointer<Handle>, Bool)>> newMediaSettingsUpdateException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>> newInvalidOutputAudioDeviceIdException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>> newSetMicrophoneVolumeException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>> newMicrophoneVolumeIsAvailableException,
-required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>> newMicrophoneVolumeException,
-required Pointer<NativeFunction<Handle Function()>> throwPanicException,
-} ) {
-dl.lookupFunction<Void Function(Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer), void Function(Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer)>('register_exception')(
-newArgumentError,
-newStateError,
-newFormatException,
-newLocalMediaInitException,
-newEnumerateDevicesException,
-newRpcClientException,
-newMediaStateTransitionException,
-newInternalException,
-newMediaSettingsUpdateException,
-newInvalidOutputAudioDeviceIdException,
-newSetMicrophoneVolumeException,
-newMicrophoneVolumeIsAvailableException,
-newMicrophoneVolumeException,
-throwPanicException,
-);}
+
+void registerFunction(
+  DynamicLibrary dl, {
+  required Pointer<
+          NativeFunction<
+              Handle Function(ForeignValue, Pointer<Utf8>, Pointer<Utf8>)>>
+      newArgumentError,
+  required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>>
+      newStateError,
+  required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>>
+      newFormatException,
+  required Pointer<
+          NativeFunction<
+              Handle Function(
+                  Int64, Pointer<Utf8>, ForeignValue, Pointer<Utf8>)>>
+      newLocalMediaInitException,
+  required Pointer<
+          NativeFunction<Handle Function(Pointer<Handle>, Pointer<Utf8>)>>
+      newEnumerateDevicesException,
+  required Pointer<
+          NativeFunction<
+              Handle Function(
+                  Int64, Pointer<Utf8>, ForeignValue, Pointer<Utf8>)>>
+      newRpcClientException,
+  required Pointer<
+          NativeFunction<Handle Function(Pointer<Utf8>, Pointer<Utf8>, Int64)>>
+      newMediaStateTransitionException,
+  required Pointer<
+          NativeFunction<
+              Handle Function(Pointer<Utf8>, ForeignValue, Pointer<Utf8>)>>
+      newInternalException,
+  required Pointer<
+          NativeFunction<Handle Function(Pointer<Utf8>, Pointer<Handle>, Bool)>>
+      newMediaSettingsUpdateException,
+  required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>>
+      newInvalidOutputAudioDeviceIdException,
+  required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>>
+      newSetMicrophoneVolumeException,
+  required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>>
+      newMicrophoneVolumeIsAvailableException,
+  required Pointer<NativeFunction<Handle Function(Pointer<Utf8>)>>
+      newMicrophoneVolumeException,
+  required Pointer<NativeFunction<Handle Function()>> throwPanicException,
+}) {
+  dl.lookupFunction<
+      Void Function(
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer),
+      void Function(
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer,
+          Pointer)>('register_exception')(
+    newArgumentError,
+    newStateError,
+    newFormatException,
+    newLocalMediaInitException,
+    newEnumerateDevicesException,
+    newRpcClientException,
+    newMediaStateTransitionException,
+    newInternalException,
+    newMediaSettingsUpdateException,
+    newInvalidOutputAudioDeviceIdException,
+    newSetMicrophoneVolumeException,
+    newMicrophoneVolumeIsAvailableException,
+    newMicrophoneVolumeException,
+    throwPanicException,
+  );
+}
