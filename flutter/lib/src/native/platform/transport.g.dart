394904d09ac5ac577466c0a2116a74aea9e7034b
logist322 <53333039+logist322@users.noreply.github.com>
2022-06-20 16:44:26 +0400

    Fix WebSocket close reason not propagated to Jason from Dart (#61)
    Additionally:
    - fix `IceTransportType` not being set when creating a new `Peer`
    - remove redundant `[skip ci]` check on CI
    - temporary pin Android NDK version to r22

--- flutter/lib/src/native/platform/transport.g.dart
+++ flutter/lib/src/native/platform/transport.g.dart
@@ -12,11 +12,17 @@ void registerFunction(
   required Pointer<NativeFunction<Void Function(Handle, Pointer<Utf8>)>> send,
   required Pointer<NativeFunction<Void Function(Handle, Int32, Pointer<Utf8>)>>
       close,
+  required Pointer<NativeFunction<Int32 Function(Handle)>> closeCode,
+  required Pointer<NativeFunction<Pointer<Utf8> Function(Handle)>> closeReason,
 }) {
-  dl.lookupFunction<Void Function(Pointer, Pointer, Pointer),
-      void Function(Pointer, Pointer, Pointer)>('register_transport')(
+  dl.lookupFunction<
+      Void Function(Pointer, Pointer, Pointer, Pointer, Pointer),
+      void Function(
+          Pointer, Pointer, Pointer, Pointer, Pointer)>('register_transport')(
     connect,
     send,
     close,
+    closeCode,
+    closeReason,
   );
 }
