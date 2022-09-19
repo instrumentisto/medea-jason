637e44be8dcc6fdee806d23589920babee4f1734
rogurotus <rogurotus@gmail.com>
2022-09-19 18:43:14 +0300

    fix test

--- tests/peer/mod.rs
+++ tests/peer/mod.rs
@@ -836,14 +836,15 @@ async fn get_traffic_stats() {
     for stat in second_peer_stats.0 {
         match stat.stats {
             RtcStatsType::InboundRtp(inbound) => {
-                match inbound.media_specific_stats {
+                let stats = match inbound.media_specific_stats {
                     RtcInboundRtpStreamMediaType::Audio { .. } => {
                         second_peer_audio_inbound_stats_count += 1
                     }
                     RtcInboundRtpStreamMediaType::Video { .. } => {
                         second_peer_video_inbound_stats_count += 1
                     }
-                }
+                };
+                Some(stats)
             }
             RtcStatsType::OutboundRtp(_) => unreachable!(
                 "Second Peer shouldn't have any OutboundRtp stats."
