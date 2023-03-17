3f775be6d137b21d22a205186c7ee6bb10c16cfb
50U10FCA7 <m.bashilov.srg@gmail.com>
2023-03-17 14:10:49 -0500

    Corrections [skip ci]

--- flutter/lib/src/native/ffi/completer.g.dart
+++ flutter/lib/src/native/ffi/completer.g.dart
@@ -1,27 +1,17 @@
 import 'dart:ffi';
-
 import 'package:ffi/ffi.dart';
-
 import 'package:medea_jason/src/native/ffi/foreign_value.dart';
-
-void registerFunction(
-  DynamicLibrary dl, {
-  required Pointer<NativeFunction<Handle Function()>> init,
-  required Pointer<NativeFunction<Void Function(Handle, ForeignValue)>>
-      complete,
-  required Pointer<NativeFunction<Void Function(Handle, Pointer<Handle>)>>
-      completeError,
-  required Pointer<NativeFunction<Handle Function(Handle)>> future,
-  required Pointer<NativeFunction<Handle Function(Int32)>> delayed,
-}) {
-  dl.lookupFunction<
-      Void Function(Pointer, Pointer, Pointer, Pointer, Pointer),
-      void Function(
-          Pointer, Pointer, Pointer, Pointer, Pointer)>('register_completer')(
-    init,
-    complete,
-    completeError,
-    future,
-    delayed,
-  );
-}
+void registerFunction(DynamicLibrary dl, {
+required Pointer<NativeFunction<Handle Function()>> init,
+required Pointer<NativeFunction<Void Function(Handle, ForeignValue)>> complete,
+required Pointer<NativeFunction<Void Function(Handle, Pointer<Handle>)>> completeError,
+required Pointer<NativeFunction<Handle Function(Handle)>> future,
+required Pointer<NativeFunction<Handle Function(Int32)>> delayed,
+} ) {
+dl.lookupFunction<Void Function(Pointer, Pointer, Pointer, Pointer, Pointer), void Function(Pointer, Pointer, Pointer, Pointer, Pointer)>('register_completer')(
+init,
+complete,
+completeError,
+future,
+delayed,
+);}
--- flutter/lib/src/native/platform/media_display_info.g.dart
+++ flutter/lib/src/native/platform/media_display_info.g.dart
@@ -1,17 +1,11 @@
 import 'dart:ffi';
-
 import 'package:ffi/ffi.dart';
-
 import 'package:medea_jason/src/native/ffi/foreign_value.dart';
-
-void registerFunction(
-  DynamicLibrary dl, {
-  required Pointer<NativeFunction<Pointer<Utf8> Function(Handle)>> deviceId,
-  required Pointer<NativeFunction<Pointer Function(Handle)>> title,
-}) {
-  dl.lookupFunction<Void Function(Pointer, Pointer),
-      void Function(Pointer, Pointer)>('register_media_display_info')(
-    deviceId,
-    title,
-  );
-}
+void registerFunction(DynamicLibrary dl, {
+required Pointer<NativeFunction<Pointer<Utf8> Function(Handle)>> deviceId,
+required Pointer<NativeFunction<Pointer Function(Handle)>> title,
+} ) {
+dl.lookupFunction<Void Function(Pointer, Pointer), void Function(Pointer, Pointer)>('register_media_display_info')(
+deviceId,
+title,
+);}
