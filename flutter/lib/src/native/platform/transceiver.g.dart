ab5e602280c909c32f04bf3aaf5691f45870512d
50U10FCA7 <m.bashilov.srg@gmail.com>
2023-03-17 14:13:19 -0500

    Corrections [skip ci]

--- flutter/lib/src/native/platform/transceiver.g.dart
+++ flutter/lib/src/native/platform/transceiver.g.dart
@@ -1,25 +1,35 @@
 import 'dart:ffi';
+
 import 'package:ffi/ffi.dart';
+
 import 'package:medea_jason/src/native/ffi/foreign_value.dart';
-void registerFunction(DynamicLibrary dl, {
-required Pointer<NativeFunction<Handle Function(Handle)>> getDirection,
-required Pointer<NativeFunction<Pointer Function(Handle)>> getSendTrack,
-required Pointer<NativeFunction<Handle Function(Handle, Handle)>> replaceTrack,
-required Pointer<NativeFunction<Handle Function(Handle)>> dropSender,
-required Pointer<NativeFunction<Bool Function(Handle)>> isStopped,
-required Pointer<NativeFunction<Pointer Function(Handle)>> mid,
-required Pointer<NativeFunction<Handle Function(Handle, Bool)>> setRecv,
-required Pointer<NativeFunction<Handle Function(Handle, Bool)>> setSend,
-required Pointer<NativeFunction<Handle Function(Handle)>> dispose,
-} ) {
-dl.lookupFunction<Void Function(Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer), void Function(Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer, Pointer)>('register_transceiver')(
-getDirection,
-getSendTrack,
-replaceTrack,
-dropSender,
-isStopped,
-mid,
-setRecv,
-setSend,
-dispose,
-);}
+
+void registerFunction(
+  DynamicLibrary dl, {
+  required Pointer<NativeFunction<Handle Function(Handle)>> getDirection,
+  required Pointer<NativeFunction<Pointer Function(Handle)>> getSendTrack,
+  required Pointer<NativeFunction<Handle Function(Handle, Handle)>>
+      replaceTrack,
+  required Pointer<NativeFunction<Handle Function(Handle)>> dropSender,
+  required Pointer<NativeFunction<Bool Function(Handle)>> isStopped,
+  required Pointer<NativeFunction<Pointer Function(Handle)>> mid,
+  required Pointer<NativeFunction<Handle Function(Handle, Bool)>> setRecv,
+  required Pointer<NativeFunction<Handle Function(Handle, Bool)>> setSend,
+  required Pointer<NativeFunction<Handle Function(Handle)>> dispose,
+}) {
+  dl.lookupFunction<
+      Void Function(Pointer, Pointer, Pointer, Pointer, Pointer, Pointer,
+          Pointer, Pointer, Pointer),
+      void Function(Pointer, Pointer, Pointer, Pointer, Pointer, Pointer,
+          Pointer, Pointer, Pointer)>('register_transceiver')(
+    getDirection,
+    getSendTrack,
+    replaceTrack,
+    dropSender,
+    isStopped,
+    mid,
+    setRecv,
+    setSend,
+    dispose,
+  );
+}
