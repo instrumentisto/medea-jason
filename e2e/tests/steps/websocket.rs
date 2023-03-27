4fe0ff62fc3bc1d4f758f51c085a24f94602fe07
logist322 <hytornoy-99@mail.ru>
2023-03-27 18:02:41 +0400

    Add some sleep

--- e2e/tests/steps/websocket.rs
+++ e2e/tests/steps/websocket.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use cucumber::{then, when};
+use tokio::time::sleep;
 
 use crate::World;
 
@@ -13,6 +16,7 @@ async fn ws_connection_restore(world: &mut World, id: String) {
     let member = world.get_member(&id).unwrap();
     member.ws_mock().disable_connection_loss().await;
     member.room().start_ws_reconnect().await.unwrap();
+    sleep(Duration::from_millis(200)).await;
 }
 
 #[then(regex = r"^(\S+)'s WS connection is lost$")]
