6f153d686b0545fb6729be7fcb3423092d622ba7
rogurotus <rogurotus@gmail.com>
2023-08-23 18:11:10 +0300

    Extend E2E demo with disable/enable remote tracks functionality (#126)
    Co-authored-by: alexlapa <lapa.alex@ex.ua>

--- e2e-demo/js/index.js
+++ e2e-demo/js/index.js
@@ -13,6 +13,8 @@ let disableAudioSend = document.getElementById('control__disable_audio_send');
 let disableVideoSend = document.getElementById('control__disable_video_send');
 let disableAudioRecv = document.getElementById('control__disable_audio_recv');
 let disableVideoRecv = document.getElementById('control__disable_video_recv');
+let disableDisplayVideoRecv = document.getElementById('control__disable_display_video_recv');
+let disableCameraDisplayVideoRecv = document.getElementById('control__disable_camera_video_recv');
 let muteAudioSend = document.getElementById('control__mute_audio_send');
 let muteVideoSend = document.getElementById('control__mute_video_send');
 let closeApp = document.getElementById('control__close_app');
@@ -479,6 +481,8 @@ window.onload = async function() {
   let isVideoSendEnabled = true;
   let isAudioRecvEnabled = true;
   let isVideoRecvEnabled = true;
+  let isDisplayVideoRecvEnabled = true;
+  let isCameraVideoRecvEnabled = true;
   let isAudioMuted = false;
   let isVideoMuted = false;
   let room = await newRoom();
@@ -613,27 +617,67 @@ window.onload = async function() {
       let remoteVideos = document.getElementsByClassName('remote-videos')[0];
       if (memberVideoDiv === undefined) {
         memberVideoDiv = document.createElement('div');
-        memberVideoDiv.classList.add('video');
-        memberVideoDiv.classList.add('d-flex');
-        memberVideoDiv.classList.add('flex-column');
-        memberVideoDiv.classList.add('align-items-center');
+        memberVideoDiv.classList.add('video', 'd-flex', 'flex-column', 'align-items-center');
         memberVideoDiv.style = 'margin: 10px';
         remoteVideos.appendChild(memberVideoDiv);
         remote_videos[remoteMemberId] = memberVideoDiv;
+
+        let recvDeviceVideo = true;
+        let buttonDeviceVideoToggleElement = document.createElement('button');
+        buttonDeviceVideoToggleElement.innerHTML = "Disable device video recv";
+        buttonDeviceVideoToggleElement.onclick = function () {
+          recvDeviceVideo = !recvDeviceVideo;
+          if (recvDeviceVideo) {
+            connection.enable_remote_video(rust.MediaSourceKind.Device);
+            buttonDeviceVideoToggleElement.innerHTML = "Disable device video recv";
+          } else {
+            connection.disable_remote_video(rust.MediaSourceKind.Device);
+            buttonDeviceVideoToggleElement.innerHTML = "Enable device video recv";
+          }
+        };
+        memberVideoDiv.appendChild(buttonDeviceVideoToggleElement);
+
+        let recvDisplayVideo = true;
+        let buttonDisplayVideoToggleElement = document.createElement('button');
+        buttonDisplayVideoToggleElement.innerHTML = "Disable display video recv";
+        buttonDisplayVideoToggleElement.onclick = function () {
+          recvDisplayVideo = !recvDisplayVideo;
+          if (recvDisplayVideo) {
+            connection.enable_remote_video(rust.MediaSourceKind.Display);
+            buttonDisplayVideoToggleElement.innerHTML = "Disable display video recv";
+          } else {
+            connection.disable_remote_video(rust.MediaSourceKind.Display);
+            buttonDisplayVideoToggleElement.innerHTML = "Enable display video recv";
+          }
+        };
+        memberVideoDiv.appendChild(buttonDisplayVideoToggleElement);
+
+        let recvAudio = true;
+        let buttonAudioToggleElement = document.createElement('button');
+        buttonAudioToggleElement.innerHTML = "Disable audio recv";
+        buttonAudioToggleElement.onclick = function () {
+          recvAudio = !recvAudio;
+          if (recvAudio) {
+            connection.enable_remote_audio();
+            buttonAudioToggleElement.innerHTML = "Disable audio recv";
+          } else {
+            connection.disable_remote_audio();
+            buttonAudioToggleElement.innerHTML = "Enable audio recv";
+          }
+        };
+        memberVideoDiv.appendChild(buttonAudioToggleElement);
       }
 
       let memberIdEl = document.createElement('span');
       memberIdEl.innerHTML = remoteMemberId;
-      memberIdEl.classList.add('member-id');
-      memberIdEl.classList.add('order-4');
+      memberIdEl.classList.add('member-id', 'order-4');
       memberVideoDiv.appendChild(memberIdEl);
 
       connection.on_quality_score_update((score) => {
         let qualityScoreEl = memberVideoDiv.getElementsByClassName('quality-score')[0];
         if (qualityScoreEl === undefined) {
           qualityScoreEl = document.createElement('span');
-          qualityScoreEl.classList.add('quality-score');
-          qualityScoreEl.classList.add('order-5');
+          qualityScoreEl.classList.add('quality-score', 'order-5');
           memberVideoDiv.appendChild(qualityScoreEl);
         }
         qualityScoreEl.innerHTML = score;
@@ -647,8 +691,7 @@ window.onload = async function() {
             playElement = memberVideoDiv.getElementsByClassName('display-video')[0];
             if (playElement === undefined) {
               playElement = document.createElement('video');
-              playElement.classList.add('display-video');
-              playElement.classList.add('order-2');
+              playElement.classList.add('display-video', 'order-2');
               playElement.playsinline = 'true';
               playElement.controls = 'true';
               playElement.autoplay = 'true';
@@ -666,8 +709,7 @@ window.onload = async function() {
             if (playElement === undefined) {
               playElement = document.createElement('video');
               playElement.className = 'camera-video';
-              playElement.classList.add('camera-video');
-              playElement.classList.add('order-1');
+              playElement.classList.add('camera-video', 'order-1');
               playElement.playsinline = 'true';
               playElement.controls = 'true';
               playElement.autoplay = 'true';
@@ -686,8 +728,7 @@ window.onload = async function() {
           if (playElement === undefined) {
             playElement = document.createElement('audio');
             playElement.className = 'audio';
-            playElement.classList.add('audio');
-            playElement.classList.add('order-3');
+            playElement.classList.add('audio', 'order-3');
             playElement.controls = 'true';
             playElement.autoplay = 'true';
             if (track.media_direction() != 0) {
@@ -943,6 +984,28 @@ window.onload = async function() {
         await room.enable_remote_video();
         isVideoRecvEnabled = true;
         disableVideoRecv.textContent = 'Disable video recv'
+      }
+    });
+    disableDisplayVideoRecv.addEventListener('click', async () => {
+      if (isDisplayVideoRecvEnabled) {
+        await room.disable_remote_video(rust.MediaSourceKind.Display);
+        isDisplayVideoRecvEnabled = false;
+        disableDisplayVideoRecv.textContent = 'Enable display video recv'
+      } else {
+        await room.enable_remote_video(rust.MediaSourceKind.Display);
+        isDisplayVideoRecvEnabled = true;
+        disableDisplayVideoRecv.textContent = 'Disable display video recv'
+      }
+    });
+    disableCameraDisplayVideoRecv.addEventListener('click', async () => {
+      if (isCameraVideoRecvEnabled) {
+        await room.disable_remote_video(rust.MediaSourceKind.Device);
+        isCameraVideoRecvEnabled = false;
+        disableCameraDisplayVideoRecv.textContent = 'Enable camera video recv'
+      } else {
+        await room.enable_remote_video(rust.MediaSourceKind.Device);
+        isCameraVideoRecvEnabled = true;
+        disableCameraDisplayVideoRecv.textContent = 'Disable camera video recv'
       }
     });
     closeApp.addEventListener('click', () => {
