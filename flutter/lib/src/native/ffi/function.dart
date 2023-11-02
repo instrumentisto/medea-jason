7917c9a519b1af17534d4f484226060a535ca513
rogurotus <rogurotus@gmail.com>
2023-11-02 12:57:54 +0300

    fmt

--- flutter/lib/src/native/ffi/function.dart
+++ flutter/lib/src/native/ffi/function.dart
@@ -1,8 +1,8 @@
 import 'dart:ffi';
 
+import '../jason.dart';
 import 'foreign_value.dart';
 import 'function.g.dart' as bridge;
-import '../jason.dart';
 
 void registerFunctions(DynamicLibrary dl) {
   bridge.registerFunction(
@@ -15,19 +15,16 @@ void registerFunctions(DynamicLibrary dl) {
 /// argument.
 void _callFn(dynamic Function(dynamic) fn, ForeignValue value) {
   try {
-    print(fn.runtimeType);
     var arg = value.toDart();
     if (arg != null) {
       var res = fn(arg);
       if (res is Future<void>) {
-        print('here');
         res.catchError((e, stack) => api.logDartException(
             message: e.toString(), stackTrace: stack.toString()));
       }
     } else {
       var res = (fn as dynamic Function())();
       if (res is Future<void>) {
-        print('here2');
         res.catchError((e, stack) => api.logDartException(
             message: e.toString(), stackTrace: stack.toString()));
       }
