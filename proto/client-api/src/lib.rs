d841177df4cdae3c768683f347697e569b79cfd1
alexlapa <lapa.alex@ex.ua>
2024-08-20 13:48:08 +0300

    Revert "for test revert later"
    This reverts commit d3b452f025772d62e242c46657a33891d51dfae6.

--- proto/client-api/src/lib.rs
+++ proto/client-api/src/lib.rs
@@ -858,8 +858,9 @@ pub struct TrackPatchEvent {
     /// Muting and unmuting can be performed without adding/removing tracks
     /// from transceivers, hence renegotiation is not required.
     pub muted: Option<bool>,
-    // /// [`EncodingParameters`] for the [`Track`] which should be patched.
-    // pub encoding_parameters: Option<Vec<EncodingParameters>>,
+
+    /// [`EncodingParameters`] for the [`Track`] which should be patched.
+    pub encoding_parameters: Option<Vec<EncodingParameters>>,
 }
 
 /// Media exchange direction of a `Track`.
@@ -912,7 +913,7 @@ impl From<TrackPatchCommand> for TrackPatchEvent {
                 }
             }),
             receivers: None,
-            // encoding_parameters: None,
+            encoding_parameters: None,
         }
     }
 }
@@ -926,7 +927,7 @@ impl TrackPatchEvent {
             muted: None,
             media_direction: None,
             receivers: None,
-            // encoding_parameters: None,
+            encoding_parameters: None,
         }
     }
 
@@ -951,9 +952,9 @@ impl TrackPatchEvent {
             self.receivers = Some(receivers.clone());
         }
 
-        // if let Some(encodings) = &another.encoding_parameters {
-        //     self.encoding_parameters = Some(encodings.clone());
-        // }
+        if let Some(encodings) = &another.encoding_parameters {
+            self.encoding_parameters = Some(encodings.clone());
+        }
     }
 }
 
@@ -1045,11 +1046,12 @@ pub struct VideoSettings {
 
     /// Source kind of these [`VideoSettings`].
     pub source_kind: MediaSourceKind,
-    // /// [`EncodingParameters`] of these [`VideoSettings`].
-    // pub encoding_parameters: Vec<EncodingParameters>,
-    //
-    // /// [`SvcSettings`] of these [`VideoSettings`].
-    // pub svc_settings: Vec<SvcSettings>,
+
+    /// [`EncodingParameters`] of these [`VideoSettings`].
+    pub encoding_parameters: Vec<EncodingParameters>,
+
+    /// [`SvcSettings`] of these [`VideoSettings`].
+    pub svc_settings: Vec<SvcSettings>,
 }
 
 /// Possible media sources of a video [`Track`].
