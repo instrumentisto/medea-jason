cfbaab10af862c65288932669eca2acef032697e
evdokimovs <evdokimovs@protonmail.ch>
2024-12-10 12:36:17 -0300

    test: uncomment wake port

--- src/platform/dart/utils/callback.rs
+++ src/platform/dart/utils/callback.rs
@@ -220,6 +220,8 @@ extern "C" fn callback_finalizer(_: *mut c_void, cb: *mut c_void) {
 pub mod tests {
     #![expect(clippy::missing_safety_doc, reason = "only for testing")]
 
+    use std::cell::RefCell;
+
     use dart_sys::Dart_Handle;
 
     use crate::api::DartValueArg;
