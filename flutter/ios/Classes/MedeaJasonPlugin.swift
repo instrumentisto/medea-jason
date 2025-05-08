35f66eebc348a981c87e597729981f0cac3ab545
alexlapa <lapa.alex@ex.ua>
2025-05-08 23:05:04 +0300

    wip

--- flutter/ios/Classes/MedeaJasonPlugin.swift
+++ flutter/ios/Classes/MedeaJasonPlugin.swift
@@ -1,14 +1,19 @@
 import Flutter
 import UIKit
 
-public class SwiftMedeaJasonPlugin: NSObject, FlutterPlugin {
+public class MedeaJasonPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "medea_jason", binaryMessenger: registrar.messenger())
-    let instance = SwiftMedeaJasonPlugin()
+    let instance = MedeaJasonPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
   }
 }
