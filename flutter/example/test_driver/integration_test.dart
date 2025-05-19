b5bf8f95365cdfa05589599ba8b7d9e8ccbaf5aa
alexlapa <lapa.alex@ex.ua>
2025-05-19 15:07:03 +0300

    wtf android

--- flutter/example/test_driver/integration_test.dart
+++ flutter/example/test_driver/integration_test.dart
@@ -8,22 +8,20 @@ const List<String> _androidPermissions = ['CAMERA', 'RECORD_AUDIO'];
 
 Future<void> main() async {
   var driver = await FlutterDriver.connect();
-  print('PERMISSIONS 000');
   if ((await driver.serviceClient.getVM()).operatingSystem == 'android') {
-    print('PERMISSIONS 111');
     for (var permission in _androidPermissions) {
-      print('PERMISSIONS 2222');
-      await Process.run('adb', [
+      var result = await Process.run('adb', [
         'shell',
         'pm',
         'grant',
         _packageName,
         'android.permission.$permission',
       ]);
+
+      print('PERMISSIONS: STDOUT: ${result.stdout}, STDERR: ${result.stderr}');
     }
-    print('PERMISSIONS 3333');
+
   }
-  print('PERMISSIONS 44444');
   await integrationDriver(driver: driver);
-  print('PERMISSIONS 5555');
+  print('PERMISSIONS END');
 }
